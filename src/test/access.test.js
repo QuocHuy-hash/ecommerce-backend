fa2aa836868fee446673b6944485d17592d3e416
FpolyQuocHuy <102716105+FpolyQuocHuy@users.noreply.github.com>
2024-04-18 20:23:35 +0700

    (features) : init test used jest

---
+++ src/test/access.test.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const {
+    login,
+    AccessService,
+    logout,
+    handleRefreshToken
+} = require('../services/access.service'); 
+// Mock data for testing
+const mockShop = {
+    email: 'huy123@gmail.com',
+    password: '123456',
+    firstName: 'Quoc',
+    lastName: 'Huy',
+    role: '["ADMIN", "SHOP"]'
+};
+
+
+const mockRefreshToken = 'mock-refresh-token';
+describe('Authentication and Access Service', () => {
+    describe('Login Function', () => {
+        it('should successfully login a user', async () => {
+            const { shop, tokens } = await login({
+                email: mockShop.email,
+                password: mockShop.password
+            });
+            expect(shop).toBeDefined();
+            expect(tokens).toBeDefined();
+        });
+
+    });
+    describe('Logout Function', () => {
+        it('should successfully logout a user', async () => {
+            const mockKeyStore = { id: '1' };
+            const result = await logout(mockKeyStore);
+            expect(result).toBeDefined();
+        });
+    });
+    describe('Handle Refresh Token Function', () => {
+        it('should throw an error for an invalid refresh token', async () => {
+            const invalidToken = 'invalid-refresh-token';
+            await expect(handleRefreshToken(invalidToken)).rejects.toThrow('Shop not registed');
+        });
+    });
+});
+
